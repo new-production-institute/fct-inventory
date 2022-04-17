597eef25681a4f98942db7aedbb24b08d7d29104
Neil Gershenfeld <gersh@cba.mit.edu>
2022-04-17 15:43:02 -0400

    wip

--- TOML-JSON.py
+++ TOML-JSON.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
 import toml,json
-
+#
+# read TOML
+#
 data = toml.load("public/inv.toml")
+#
+# write JSON
+#
 f = open("public/inv.json","w")
 json.dump(data,f)
 f.close()
